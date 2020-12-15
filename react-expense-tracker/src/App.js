c595d015668231d1c70e18073a433a001a148fe4
rabinabdian <rabin.abdian@gmail.com>
2020-12-15 21:01:41 +0200

    added functionality and components project completed

--- react-expense-tracker/src/App.js
+++ react-expense-tracker/src/App.js
@@ -5,6 +5,8 @@ import { Header } from "./components/Header";
 import { GlobalProvider } from "./context/GlobalState";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 
 function App() {
@@ -14,8 +16,8 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
-        {/* <TransactionList />
-        <AddTransaction /> */}
+         <TransactionList />
+       <AddTransaction />
       </div>
     </GlobalProvider>
   );
